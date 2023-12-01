523f9a26eee27a98653c8067785522c70c735edf
Daniyal9988 <saeed.s@graduate.utm.my>
2023-12-01 17:00:10 +0800

    this is login page

---
+++ src/Login/login.js
@@ -0,0 +1,46 @@
+import React from "react";
+import Logo from "../images/propert.png";
+import "./Login.css"
+import Nav from "../components/Navbar"
+
+export default function Login() {
+  return (
+  <div>
+    <div>
+      <Nav />
+    </div>
+    <div className="login-box">
+      <a href="https://www.youtube.com" > <img className="sign" src={Logo} alt="Logo" /></a>
+      <h2>Sign In</h2>
+      <hr className="horizontal-line" />
+
+      <form>
+        <div className="user-box">
+          <label className="dany">User ID</label>
+          <input  type="text" name="" required />
+        </div>
+
+        <div className="user-box">
+          <label className="head">Password</label>
+          <input  type="password" name="" required />
+        </div>
+
+        <div style={{ display: 'grid', gridTemplateColumns: '200px 1fr' }}>
+          <div>
+            Don't have a account?<a href="https://www.youtube.com" target="_blank" rel="noopener noreferrer">Signup</a>
+          </div>
+          <div>
+            <button className="submit-button" type="submit">
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              Login
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
+  </div>
+  );
+}
