333a5de855eaa634f1d8b97b7f0a0e2eb5aa6df4
Daniyal9988 <saeed.s@graduate.utm.my>
2023-12-01 17:04:38 +0800

    this is navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,19 @@
+import React from "react"
+import myImage from "../images/react-icon-small.png";
+import "./style.css"
+
+export default function Navbar() {
+    return (
+        <nav>
+            <img class="nav--icon" src={myImage} alt="" />
+            <h3 className="nav--logo_text">Al jazeera company</h3>
+            <ul class="nav-bar-homepage">
+                <li style={{ marginRight: "20px" , marginTop: "10px"}}><a  className="icon" href="#" style={{ textDecoration: "none", color: "#fff" }}>Home</a></li>
+                <li  style={{ marginRight: "20px" , marginTop: "10px"}}><a  className="icon" href="#" style={{ textDecoration: "none", color: "#fff" }}>Contact Us</a></li>
+                <li style={{ marginRight: "20px" , marginTop: "10px"}}><a  className="icon" href="#" style={{ textDecoration: "none", color: "#fff" }}>Properties</a></li>
+                <li ><button class="signup_button_homepage" href="#">Sign In</button> </li>
+            </ul>
+        </nav>
+    )
+}
+
